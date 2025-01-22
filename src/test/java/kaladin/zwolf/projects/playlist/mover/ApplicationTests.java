d235e80e1b6a1851a8ed33cb4789f3dd740d2c37
Kaladin12 <eliancruz998@gmail.com>
2025-01-21 22:13:51 -0800

    initial commit

---
+++ src/test/java/kaladin/zwolf/projects/playlist/mover/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kaladin.zwolf.projects.playlist.mover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
